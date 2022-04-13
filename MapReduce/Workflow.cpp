cf0b1d0fd787ec07b377af081dda97175e6e9bad
Colton Wilson <71854794+ColtonWilson@users.noreply.github.com>
2022-04-13 12:43:43 -0700

    Merged both programs
    Got both programs to work. I am going to try and move some of the commands in the while loop you made to the FileManagement. Just so it looks cleaner on the workflow. 
    I did a test with 20 paragraphs and results look promising. (Those can be seen in the test2.txt files). I did notice one bug that the tokenizer will not pick up any words that have accent over the letter. It also will take words like it's and make it ("it") 1 and ("s") 1.

--- MapReduce/Workflow.cpp
+++ MapReduce/Workflow.cpp
@@ -41,6 +41,8 @@ WorkFlow::WorkFlow() {}
 //WorkFlow constructor with three parameters
 WorkFlow::WorkFlow(string inputFile, string intermediateFile, string outputFile) : inputFileLocation{ inputFile }, intermediateFileLocation{ intermediateFile }, outputFileLocation{ outputFile }
 {
+	//<-----------------Part 1------------------------------------------>
+	// Writing from input file to intermediate file
 	//Create an input and output stream class
 	ifstream inputFileStream;
 	ofstream intermediateFileStream;
@@ -66,6 +68,81 @@ WorkFlow::WorkFlow(string inputFile, string intermediateFile, string outputFile)
 			Map toTempFile(intermediateFile, data);
 		}
 
+	}
+	FileStreamSystem.closeInputFile(inputFileStream);
+
+	//<-----------------Part 2------------------------------------------>
+	// Sorting and Reducing
+	// declare and initialize local variables.
+	string intermediateFilePath = intermediateFile; // intermediate file path
+	string outputFilePath = outputFile;
+	string* intermediateFilePathPntr = &intermediateFilePath;
+	string* outputFilePathPntr = &outputFilePath;
+
+	// initialize local variables
+	ofstream outputFileStream;
+
+	// open the file for editing.
+	FileStreamSystem.clearFile(outputFileStream, outputFile);
+
+	// create an instance of the Sorting class.
+	Sorting sortingObj(intermediateFilePathPntr);
+
+	// create an instance of the Reduce class.
+	Reduce reduceObj(outputFilePathPntr);
+
+	// format the file.
+	sortingObj.format();
+
+	// Create local variables. Input file stream object
+	ifstream inputFileStreamObj;
+	string line;
+	string entryString{ NULL };
+	string* entryStrPntr{ NULL };
+
+	// create a delimiter to find the following string: ")"
+	string closedParenthesis{ ")" };
+	string openParenthesis{ "(" };
+	size_t openPos{ NULL };
+	size_t closedPos{ NULL };
+
+	// open the intermediate file
+	FileStreamSystem.openFileInstream(inputFileStreamObj,*intermediateFilePathPntr);
+	
+	// assign the entry string pointer
+	entryStrPntr = &entryString;
+
+	// get the first line from the file.
+	while (getline(inputFileStreamObj, line)) {
+
+		// find the position of the first open parenthesis
+		openPos = line.find(openParenthesis);
+
+		// offset for the find method of the string class.
+		size_t offset{ 0 };
+
+		// while there are words in this line, keep extracting them.
+		while (openPos != string::npos) {
+
+			// find the position of the closed parenthesis
+			closedPos = line.find(closedParenthesis, offset);
+
+			// if the open and closed parentheses were found, pass the substring to the reduce class.
+			if ((openPos != string::npos) && (closedPos != string::npos)) {
+
+				// extract the entry. 
+				entryString = line.substr(openPos, (closedPos - openPos) + 1);
+
+				// pass the string to the reduce method from the Reduce class.
+				reduceObj.reduce(entryStrPntr);
+			}
+
+			// update the offset into the line for the next search.
+			offset = closedPos + 1;
+
+			// Find the position of the next open parenthesis. 
+			openPos = line.find(openParenthesis, offset);
+		}
 	}
 	
 }
