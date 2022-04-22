b04c282ff23364a967bf8ee57a5e093bf559ae42
anthonyredamonti <39130075+anthonyredamonti@users.noreply.github.com>
2022-04-22 15:45:11 -0400

    Update Workflow.h
    Added helpful messages to user.

--- MapReduce/Workflow.h
+++ MapReduce/Workflow.h
@@ -146,9 +146,6 @@ public:
 	//Return the value in outputFileDirectoryLocation
 	const string getOutputFileDirectoryLocation(void);
 	
-    // Display the progress to the user (every 5%)
-	void displayProgress(double inputPercentage);
-	
 	//**********Destructor*********
 	~Workflow();
 	
