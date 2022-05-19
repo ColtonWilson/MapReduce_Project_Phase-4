6b2f1e409e889dbb26be8af57826bb2b84696367
anthonyredamonti <39130075+anthonyredamonti@users.noreply.github.com>
2022-05-18 22:03:59 -0400

    Update FileManagement.h
    Added function for reading an entire line from a file. (readFromFile(ifstream& fileStream, const string& userFile);

--- MapProcess/MapProcess/FileManagement.h
+++ MapProcess/MapProcess/FileManagement.h
@@ -4,7 +4,7 @@ Professor Scott Roueche
 CSE 687 Object Oriented Design
 Syracuse University
 Project 1
-4/9/2022
+5/18/2022
 
 FileManagement.h
 
@@ -56,7 +56,7 @@ public:
 	//default constructor
 	FileManagement();
 
-	
+
 	//**********Member Functions**********
 	vector<string> getAllFilesInDir(const string& dirPath, const vector<string> dirSkipList = { });
 
@@ -65,56 +65,54 @@ public:
 	//Open the directory files that user has given to be able to read
 	void openFileInstream(ifstream& fileStream, const string userFile);
 
-	
+
 	//Pre-condition: File stream has been created and file Directory has been given
 	//Post-condition:Files have been opened as an out stream
 	//Open the directory files that user has given and able to read
 	void openFileOutstream(ofstream& fileStream, const string& userFile);
 
-	
+
 	//Pre-condition:  File stream has been created and file is open
 	//Post-condition:Failure message will print to screen if file is corrupt. 
 	//Check to see if file failed to open
 	void fileCorrupt(ifstream& fileStream);
 
-	
+
 	//Pre-condition: File stream has been created and file is open
 	//Post-condition:File has been closed
 	//Close the directory files that user has given
 	void closeInputFile(ifstream& fileToClose);
-	
+
 	//Pre-condition: File stream has been created and file is open
 	//Post-condition:File has been closed
 	//Close the directory files that user has given
 	void closeOutputFile(ofstream& fileToClose);
-	
+
 	//Pre-Condition:File stream has been created and file is open
 	//Post-Condition:File is update with string data
 	//Write into a temporary file
 	void writeToTempFile(ofstream& fileStream, const string& data);
-	
+
 	//Pre-Condition:
 	//Post-Condition:
 	//
 	void writeToOutputFile(ofstream& fileStream, string key, size_t sum);
-	
+
 	//Pre-Condition:File stream has been created and file is open
 	//Post-Condition: string data is updated with a line of raw data from input file
 	//grab a line of data from the input file
-	void readFromFile(ifstream& fileStream, string& data, const int& Nline);
+	void readFromFile(ifstream& fileStream, string& data);
 
-
-	
 	//Pre-Condition:File stream has been created
 	//Post-Condition:File is empty
 	//clear the contents of a file
 	void clearFile(ofstream& fileStream, const string& userFile);
-	
+
 	//**********Destructor*********
 	~FileManagement();
 
 private:
-	
+
 
 
 };
