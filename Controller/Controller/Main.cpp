1fee529a8257239b851df90204f7abf622023a20
anthonyredamonti <39130075+anthonyredamonti@users.noreply.github.com>
2022-06-07 15:29:47 -0400

    Update Main.cpp
    Controller will start the stub processes. Then the controller will start collecting heartbeat messages from the Map and Reduce processes.

--- Controller/Controller/Main.cpp
+++ Controller/Controller/Main.cpp
@@ -1,13 +1,34 @@
+#define  _WIN32_WINNT   0x0601
+
 #include <iostream>
 #include <string>
+#include <thread>
 #include <WS2tcpip.h>
+#include <vector>
 #pragma comment(lib, "ws2_32.lib")
 
-using namespace std;
+#include "Heartbeat.h"
 
+using std::cerr;
+using std::cout;
+using std::endl;
+using std::string;
+using std::vector;
+using std::thread;
+
+// function declarations.
+int startStub1();
+int startStub2();
 
 void main()
 {
+	// create a heartbeat object.
+	Heartbeat heartbeatObj;
+
+	// create both stub processes.
+	if (startStub1() < 0) { return; }
+	if (startStub2() < 0) { return; }
+
 	cout << "Welcome to the MapReduce Controller" << endl;
 
 	//*******************************First Socket***********************************
@@ -53,41 +74,26 @@ void main()
 		return;
 	}
 
-
-
 	//Do-While loop to send and receive data
 	char buf[4096];
 	string userInput;
 	string returnMessage = "";
 
-	do
+	userInput = "2 0";
+
+	if (userInput.size() > 0)		//Make sure user has typed in something
 	{
-		userInput = "2 0";
-
-		if (userInput.size() > 0)		//Make sure user has typed in something
+		//send the text (begin mapping)
+		int sendResult = send(sock, userInput.c_str(), userInput.size() + 1, 0);
+		if (sendResult != SOCKET_ERROR)
 		{
-			//send the text
-			int sendResult = send(sock, userInput.c_str(), userInput.size() + 1, 0);
-			if (sendResult != SOCKET_ERROR)
-			{
-				//wait for response
-				ZeroMemory(buf, 4096);
-				int bytesRecieved = recv(sock, buf, 4096, 0);
-				if (bytesRecieved > 0)
-				{
-					returnMessage = string(buf, 0, bytesRecieved);
-					//Echo the response
-					cout << "Stub1> " << returnMessage  << endl;
-				}
-			}
+			heartbeatObj.waitForMapToFinish();
 		}
-	} while (returnMessage.size() == 0);
-
+	}
 
 	//Gracefully close down everything
 	closesocket(sock);
 	WSACleanup();
-
 
 	//*******************************Second Socket***********************************
 
@@ -129,35 +135,116 @@ void main()
 		return;
 	}
 
+	userInput = "2 1";
 
-
-	//Do-While loop to send and receive data
-
-	do
+	if (userInput.size() > 0)		//Make sure user has typed in something
 	{
-		userInput = "2 1";
-
-		if (userInput.size() > 0)		//Make sure user has typed in something
+		//send the text
+		int sendResult = send(sock, userInput.c_str(), userInput.size() + 1, 0);
+		if (sendResult != SOCKET_ERROR)
 		{
-			//send the text
-			int sendResult = send(sock, userInput.c_str(), userInput.size() + 1, 0);
-			if (sendResult != SOCKET_ERROR)
-			{
-				//wait for response
-				ZeroMemory(buf, 4096);
-				int bytesRecieved = recv(sock, buf, 4096, 0);
-				if (bytesRecieved > 0)
-				{
-					returnMessage = string(buf, 0, bytesRecieved);
-					//Echo the response
-					cout << "Stub2> " << returnMessage << endl;
-				}
-			}
+			heartbeatObj.waitForReduceToFinish();
 		}
-	} while (returnMessage.size() == 0);
+	}
 
 	//Gracefully close down everything
 	closesocket(sock);
 	WSACleanup();
 
+}
+
+// start the stub 1 process.
+int startStub1() {
+	//************Start the process**************
+	// 
+	STARTUPINFO si;
+	PROCESS_INFORMATION pi;
+
+	// create an array to hold an integer in string form.
+	wchar_t wCharArray[10];
+
+	// local variable for iterating through character array.
+	int index{ 0 };
+
+	// end the string with the null character
+	wCharArray[index] = 0;
+
+	// convert the process number in string form to LPWSTR
+	LPWSTR allArgsLpwstr = wCharArray;
+
+	ZeroMemory(&si, sizeof(si));
+	si.cb = sizeof(si);
+	ZeroMemory(&pi, sizeof(pi));
+
+	// Start the child process. 
+	if (!CreateProcess(
+		L"C:\\Users\\antho\\OneDrive\\Documents\\Projects\\Stub1\\x64\\Debug\\Stub1.exe",
+		allArgsLpwstr,  // Command line
+		NULL,           // Process handle not inheritable
+		NULL,           // Thread handle not inheritable
+		FALSE,          // Set handle inheritance to FALSE
+		0,              // No creation flags
+		NULL,           // Use parent's environment block
+		NULL,           // Use parent's starting directory 
+		&si,            // Pointer to STARTUPINFO structure
+		&pi)            // Pointer to PROCESS_INFORMATION structure
+		)
+	{
+		printf("CreateProcess failed (%d).\n", GetLastError());
+		return -1;
+	}
+
+	// Close process and thread handles. 
+	CloseHandle(pi.hProcess);
+	CloseHandle(pi.hThread);
+
+	return 0;
+}
+
+// start the stub 2 process.
+int startStub2() {
+	//************Start the process**************
+	// 
+	STARTUPINFO si;
+	PROCESS_INFORMATION pi;
+
+	// create an array to hold an integer in string form.
+	wchar_t wCharArray[10];
+
+	// local variable for iterating through character array.
+	int index{ 0 };
+
+	// end the string with the null character
+	wCharArray[index] = 0;
+
+	// convert the process number in string form to LPWSTR
+	LPWSTR allArgsLpwstr = wCharArray;
+
+	ZeroMemory(&si, sizeof(si));
+	si.cb = sizeof(si);
+	ZeroMemory(&pi, sizeof(pi));
+
+	// Start the child process. 
+	if (!CreateProcess(
+		L"C:\\Users\\antho\\OneDrive\\Documents\\Projects\\Stub2\\x64\\Debug\\Stub2.exe",
+		allArgsLpwstr,  // Command line
+		NULL,           // Process handle not inheritable
+		NULL,           // Thread handle not inheritable
+		FALSE,          // Set handle inheritance to FALSE
+		0,              // No creation flags
+		NULL,           // Use parent's environment block
+		NULL,           // Use parent's starting directory 
+		&si,            // Pointer to STARTUPINFO structure
+		&pi)            // Pointer to PROCESS_INFORMATION structure
+		)
+	{
+		printf("CreateProcess failed (%d).\n", GetLastError());
+		return -1;
+	}
+
+	// Close process and thread handles. 
+	CloseHandle(pi.hProcess);
+	CloseHandle(pi.hThread);
+
+	return 0;
 }
